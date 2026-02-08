d80e461579714b9be36cef1e9fbf44e00502aa80
Chritianregis <christiancheukap@gmailcom>
2026-02-08 04:41:03 +0100

    Finition de la version avec Map

--- MapVersion/controllers/UserController.java
+++ MapVersion/controllers/UserController.java
@@ -14,7 +14,7 @@ public class UserController {
         System.out.println("****** Bienvenue sur notre platforme de gestion des utilisateurs ******");
         while (true) {
             System.out.println(
-                    "Voici les options : \n1.Ajouter un utilisateur\n2.Recuperer un utlisateur\n3.Supprimer un utilisatueur\n##.Quitter");
+                    "Voici les options : \n1.Ajouter un utilisateur\n2.Recuperer un utlisateur\n3.Supprimer un utilisatueur\n4.Afficher tous les utilisateurs\n##.Quitter");
             choix = scanner.next();
             switch (choix) {
                 case "1":
@@ -32,6 +32,17 @@ public class UserController {
                     System.out.println("Entrez l'ID de l'utilisateur");
                     Long userID = scanner.nextLong();
                     System.out.println(userService.getUser(userID).toString());
+                    break;
+                case "3":
+                    System.out.println("Entrez l'ID de l'utilisateur");
+                    Long userIDToDelete = scanner.nextLong();
+                    userService.deleteUser(userIDToDelete);
+                    break;
+                case "4":
+                    System.out.println("Liste de tous les utilisateurs :");
+                    for (User u : userService.getAllUsers()) {
+                        System.out.println(u.toString());
+                    }
                     break;
                 case "##":
                     System.out.println("Merci pour votre utilisation !");
